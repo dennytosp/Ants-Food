fd712e1a699ac247361136fc4acea383312c4353
Dinh Tien Phong <ken.phong1802@gmail.com>
2021-09-18 16:38:29 +0700

    Project-start

---
+++ src/assets/data/sources.js
@@ -0,0 +1,25 @@
+import {COLORS} from '../../constants';
+import {photos} from '../../assets';
+
+export const ONBOARD_DATA = [
+  {
+    title: 'Save time by tracking your studies',
+    text: 'Schedule your classes, assignments, quizzes and more',
+    image: photos.onboard1,
+  },
+  {
+    title: 'Stay on top of your education',
+    text: 'Reduce your stress, increase your productivity',
+    image: photos.onboard2,
+  },
+  {
+    title: 'Spend more time doing the things you love',
+    text: 'Get started within five minutes',
+    image: photos.onboard3,
+  },
+];
+
+const sources = {
+  ONBOARD_DATA,
+};
+export default sources;
---
+++ src/assets/img.js
@@ -0,0 +1,28 @@
+import onboard1 from './images/photos/onboard/Onboard1.png';
+import onboard2 from './images/photos/onboard/Onboard2.png';
+import onboard3 from './images/photos/onboard/Onboard3.png';
+import home from './images/icons/home.png';
+import bag from './images/icons/bag.png';
+import favorite from './images/icons/heart.png';
+import profile from './images/icons/profile.png';
+import searching from './images/icons/search.png';
+import cart from './images/icons/cart.png';
+import product from './images/icons/product.png';
+
+export const photos = {
+  onboard1,
+  onboard2,
+  onboard3,
+};
+export const icons = {
+  home,
+  bag,
+  favorite,
+  profile,
+  searching,
+  cart,
+  product,
+};
+
+const img = {photos, icons};
+export default img;
